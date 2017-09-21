88e60d4914c4bb1c03d29c6f81e0f6ffbd049bb4
Markus Malz <markus.malz@it2media.de>
2017-09-21 14:06:06 +0200

    Zwischenstand

---
+++ XamGtkExplorer.GTK/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.GTK;
+
+namespace XamGtkExplorer.GTK
+{
+    public class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+            Gtk.Application.Init();
+            Forms.Init();
+            var app = new App();
+            var window = new FormsWindow();
+            window.LoadApplication(app);
+            window.SetApplicationTitle("WeatherApp");
+            window.Show();
+            Gtk.Application.Run();
+        }
+    }
+}
